14bbf6cb3aabfdcdbb83e6784b52052fe58ed39e
Banjos <brunosilveiradosanjos@gmail.com>
2025-03-15 20:57:05 -0300

    feat: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {  
+  content: [
+    "./projects/angular-app/src/**/*.{html,ts}",
+    "./projects/lib-turbo-components-scd/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
